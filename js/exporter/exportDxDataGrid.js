86a18fdfc0b2bce95ebafa92a0a262b4b95693c1
Vladimir Kuznetsov <kuznetsov.vladimir.n@gmail.com>
2020-07-09 23:13:43 +0300

    dx generic light theme by default

--- js/exporter/exportDxDataGrid.js
+++ js/exporter/exportDxDataGrid.js
@@ -2,10 +2,14 @@ var exportDataGrid = (function () {
 
     function exportToPDF(pdfDoc, component, customizeCell, autoTableOptions) {
         const defaultAutoTableOptions = {
-            theme: 'plain',
             startY: 15
         };
+
         autoTableOptions = $.extend({}, defaultAutoTableOptions, autoTableOptions);
+
+        if (!autoTableOptions.theme) {
+            autoTableOptions = $.extend({}, autoTableOptions, getDxThemeStyles());
+        }
 
         const dataProvider = component.getDataProvider(component.getController("export")._selectionOnly);
 
@@ -51,7 +55,7 @@ var exportDataGrid = (function () {
                     for (let cellIndex = 0; cellIndex < columns.length; cellIndex++) {
                         const cellData = dataProvider.getCellData(rowIndex, cellIndex, true);
                         const cell = cellData.cellSourceData;
-                        const rowType = cellData.cellSourceData.rowType;
+                        const rowType = cell.rowType;
 
                         var pdfCell = {
                             content: cellData.value,
@@ -87,53 +91,46 @@ var exportDataGrid = (function () {
                                 }
                             }
 
-                            internalDrawCell = function (data) {
-                                var noTheme = data.settings.theme === 'plain';
-                                if (!noTheme) {
-                                    var lineWidth = data.cell.styles.lineWidth;
-                                    if (lineWidth > 0) {
-                                        var isFirst = data.column.index === 0;
-                                        if (!isFirst) {
-
-                                            var fillColor = data.cell.styles.fillColor;
-                                            var lineColor = data.cell.styles.lineColor;
-
-                                            var x = data.cursor.x;
-                                            var y = data.cursor.y;
-                                            var w = data.cell.width;
-                                            var h = data.cell.height;
-
-                                            data.doc.setDrawColor(fillColor);
-                                            data.doc.setFillColor(fillColor);
-                                            data.doc.rect(x - 0.3, y + lineWidth, 0.6, h - lineWidth * 2, "FD");
-
-                                            data.doc.setDrawColor(lineColor);
-                                            data.doc.line(x - 0.3, y, x + 0.3, y);
-                                            data.doc.line(x - 0.3, y + h, x + 0.3, y + h);
-                                        }
-                                    }
-                                }
-                            };
+                            if (autoTableOptions.theme === 'grid') {
+                                internalDrawCell = function (data) {
+                                    patchBordersForGroupAndFooterCells(data);
+                                };
+                            }
                         }
 
                         // Assing style from dxDataGrid
 
+                        if (autoTableOptions.useDxTheme) {
+                            customizeDxThemeCell(pdfCell, cell, { cellIndex: cellIndex, rowLength: columns.length, dxThemeConfig: getDxThemeConfig() });
+                            internalDrawCell = function (data) {
+                                drawDxThemeCell(data, {
+                                    gridCell: cell,
+                                    dxThemeConfig: getDxThemeConfig()
+                                });
+                            };
+                        }
+
                         //styles
-                        var style = styles[dataProvider.getStyleId(rowIndex, cellIndex)];
-                        if (style) {
-                            //debugger;
-                            if (style.alignment)
-                                pdfCell.styles.halign = style.alignment;
-                            if (style.bold)
-                                pdfCell.styles.fontStyle = 'bold';
-                            if (style.format) {
-                                // 
-                            }
-                            if (style.wrapText) {
-                                pdfCell.styles.cellWidth = 'wrap';
-                            }
-                            if (style.dataType) {
-                                // 
+                        if (rowType === 'header') {
+                            if (columns[cellIndex].alignment)
+                                pdfCell.styles.halign = columns[cellIndex].alignment;
+                        }
+                        else {
+                            var style = styles[dataProvider.getStyleId(rowIndex, cellIndex)];
+                            if (style) {
+                                if (style.alignment)
+                                    pdfCell.styles.halign = style.alignment;
+                                if (style.bold)
+                                    pdfCell.styles.fontStyle = 'bold';
+                                if (style.format) {
+                                    // 
+                                }
+                                if (style.wrapText) {
+                                    pdfCell.styles.cellWidth = 'wrap';
+                                }
+                                if (style.dataType) {
+                                    // 
+                                }
                             }
                         }
 
@@ -151,7 +148,7 @@ var exportDataGrid = (function () {
                         }
                         else {
                             if (rowType === 'group') {
-                                if (pdfCell.content !== "")
+                                if (pdfCell.content !== "" || row.length > 1)
                                     row.push(pdfCell);
                             }
                             else {
@@ -201,12 +198,184 @@ var exportDataGrid = (function () {
                         var customDrawFunc = customMatrix[data.row.index][data.column.index];
                         if (customDrawFunc)
                             customDrawFunc(data);
-                    }
+                    },
+                    didDrawPage: endPageDxThemeCell
                 },
                 autoTableOptions);
 
             pdfDoc.autoTable(pdfDocOptions);
         });
+    }
+
+    function getDxThemeConfig() {
+        return {
+
+            styles: {
+                textColor: 51,
+                lineColor: 149,
+                lineWidth: 0.1
+            },
+            headCellStyles: {
+                fontStyle: 'normal',
+                textColor: 149,
+                minCellHeight: 8,
+                valign: 'bottom'
+            },
+            bodyCellStyles: {},
+            footCellStyles: {},
+            groupCellStyles: {},
+            groupFooterCellStyles: {},
+            totalFooterCellStyles: {
+                minCellHeight: 11,
+                valign: 'middle'
+            },
+            getTextColor: function (gridCell, cellIndex, rowLength) {
+                if (gridCell.rowType === 'group') {
+                    if (cellIndex === 0)
+                        return 149;
+                }
+                return undefined;
+            },
+            getFillColor: function (gridCell, cellIndex, rowLength, borderSide) {
+                if (gridCell.rowType === 'group')
+                    return 247;
+                return undefined;
+            },
+            getLineColor: function (gridCell, cellIndex, rowLength,  borderSide) {
+                return this.styles.lineColor;
+            },
+            getLineWidth: function (gridCell, cellIndex, rowLength, borderSide) {
+                if (['data', 'group', 'groupFooter', 'totalFooter'].some(t => t === gridCell.rowType)) {
+                    //var isFirst = cellIndex === 0;
+                    //var isLast = cellIndex === rowLength - 1;
+
+                    //if (borderSide === 'left' || borderSide === 'right') {
+                    //    return 0;
+                    //}
+                    return 0;
+                }
+
+                return undefined;
+            }
+        };
+    }
+
+    function getDxThemeStyles() {
+        var config = getDxThemeConfig();
+        return {
+            theme: 'plain',
+            useDxTheme: true,
+            styles: $.extend({}, config.styles),
+            headStyles: $.extend({}, config.headCellStyles),
+            bodyStyles: $.extend({}, config.bodyCellStyles),
+            footStyles: $.extend({}, config.footCellStyles)
+        };
+    }
+
+    function customizeDxThemeCell(pdfCell, gridCell, options) {
+        var config = options.dxThemeConfig;
+
+        var configStyles = {
+            group: config.groupCellStyles,
+            groupFooter: config.groupFooterCellStyles,
+            totalFooter: config.totalFooterCellStyles
+        };
+
+        if (configStyles[gridCell.rowType])
+            pdfCell.styles = $.extend({}, pdfCell.styles, configStyles[gridCell.rowType]);
+
+
+        var dynamicStyles = {
+            textColor: config.getTextColor(gridCell, options.cellIndex, options.rowLength),
+            fillColor: config.getFillColor(gridCell, options.cellIndex, options.rowLength),
+            lineWidth: config.getLineWidth(gridCell, options.cellIndex, options.rowLength)
+        };
+
+        if (dynamicStyles.textColor !== undefined)
+            pdfCell.styles.textColor = dynamicStyles.textColor;
+
+        if (dynamicStyles.fillColor !== undefined)
+            pdfCell.styles.fillColor = dynamicStyles.fillColor;
+
+        if (dynamicStyles.lineWidth !== undefined)
+            pdfCell.styles.lineWidth = dynamicStyles.lineWidth;
+    }
+
+    function drawDxThemeCell(hookData, options) {
+        if (['data', 'group', 'groupFooter', 'totalFooter'].some(t => t === options.gridCell.rowType)) {
+            var doc = hookData.doc;
+            var cell = hookData.cell;
+
+            var prevLineColor = doc.getDrawColor();
+            var lineColor = options.dxThemeConfig.getLineColor(options.gridCell, hookData.column.index, hookData.row.raw.length);
+            doc.setDrawColor(lineColor);
+
+            if (options.gridCell.rowType === 'data') {
+                // left border
+                doc.line(cell.x, cell.y, cell.x, cell.y + cell.height);
+
+                // right border
+                doc.line(cell.x + cell.width, cell.y, cell.x + cell.width, cell.y + cell.height);
+            }
+            else {
+                var isFirstCell = hookData.cell.raw === hookData.row.raw[0];
+                var isLastCell = hookData.cell.raw === hookData.row.raw[hookData.row.raw.length - 1];
+
+                // left border
+                if (isFirstCell)
+                    doc.line(cell.x, cell.y, cell.x, cell.y + cell.height);
+
+                // right border
+                if (isLastCell)
+                    doc.line(cell.x + cell.width, cell.y, cell.x + cell.width, cell.y + cell.height);
+
+                // top border
+                doc.line(cell.x, cell.y, cell.x + cell.width, cell.y);
+
+                // bottom border
+                doc.line(cell.x, cell.y + cell.height, cell.x + cell.width, cell.y + cell.height);
+            }
+
+            doc.setDrawColor(prevLineColor);
+        }
+    }
+
+    function endPageDxThemeCell(hookData) {
+        var doc = hookData.doc;
+        var margin = hookData.settings.margin;
+        var pageSize = doc.internal.pageSize;
+        var pageWidth = pageSize.width ? pageSize.width : pageSize.getWidth();
+
+        doc.line(margin.left, hookData.cursor.y, pageWidth - margin.right, hookData.cursor.y);
+    }
+
+    function patchBordersForGroupAndFooterCells(hookData) {
+        var lineWidth = hookData.cell.styles.lineWidth;
+        if (lineWidth > 0) {
+            var doc = hookData.doc;
+
+            var column = hookData.column;
+            var cell = hookData.cell;
+            var cursor = hookData.cursor;
+
+            var fillColor = cell.styles.fillColor;
+            var lineColor = cell.styles.lineColor;
+
+            var isFirst = column.index === 0;
+            if (!isFirst) {
+                var prevLineColor = doc.getDrawColor();
+
+                doc.setDrawColor(fillColor);
+                doc.setFillColor(fillColor);
+                doc.rect(cursor.x - 0.3, cursor.y + lineWidth, 0.6, cell.height - lineWidth * 2, "FD");
+
+                doc.setDrawColor(lineColor);
+                doc.line(cursor.x - 0.3, cursor.y, cursor.x + 0.3, cursor.y);
+                doc.line(cursor.x - 0.3, cursor.y + cell.height, cursor.x + 0.3, cursor.y + cell.height);
+
+                doc.setDrawColor(prevLineColor);
+            }
+        }
     }
 
     function tryGetMergeRange(rowIndex, cellIndex, mergedCells, dataProvider) {
